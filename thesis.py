d8e26036b38f38c0f7727954dfadcd2075e3b968
Veysel ÖREN <orenveysel@outlook.com>
2021-12-29 16:09:12 +0300

    Create thesis.py

---
+++ thesis.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+
+camera = cv2.VideoCapture(0)
+while True:
+    x,y = camera.read()
+    cv2.imshow("Capture", y)
+
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
+
+camera.release()
+cv2.destroyAllWindows()
+
+
+
