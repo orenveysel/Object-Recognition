6960d53a402cda5349aab25fc5f4cb81dc98f1e6
Veysel ÖREN <orenveysel@outlook.com>
2022-01-01 17:33:46 +0300

    Update thesis.py

--- thesis.py
+++ thesis.py
@@ -5,13 +5,10 @@ import numpy as np
 camera = cv2.VideoCapture(0)
 while True:
     x,y = camera.read()
-    cv2.imshow("Capture", y)
+    cv2.imshow("Object Recognition", y)
 
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
 
 camera.release()
 cv2.destroyAllWindows()
-
-
-
