2942e0e78bd17ab0e092f2e3066a97d7b3f0053f
Veysel ÖREN <orenveysel@outlook.com>
2022-01-01 20:46:35 +0300

    Update thesis.py

--- thesis.py
+++ thesis.py
@@ -1,14 +1,33 @@
 import cv2
 import numpy as np
 
+# Create a VideoCapture object and read from input file
+# If the input is the camera, pass 0 instead of the video file name
+cap = cv2.VideoCapture(0)
 
-camera = cv2.VideoCapture(0)
-while True:
-    x,y = camera.read()
-    cv2.imshow("Object Recognition", y)
+# Check if camera opened successfully
+if cap.isOpened() is False:
+    print("Error opening video stream or file")
 
-    if cv2.waitKey(25) & 0xFF == ord('q'):
+# Read until video is completed
+while cap.isOpened():
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    if ret is True:
+        # Display the resulting frame
+        cv2.imshow('Object Recognition', frame)
+
+        # Press Q on keyboard to  exit
+
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+
+    # Break the loop
+    else:
         break
 
-camera.release()
+# When everything done, release the video capture object
+cap.release()
+
+# Closes all the frames
 cv2.destroyAllWindows()
